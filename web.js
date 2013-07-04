51e998e1510cd08b30f5c143482d3e19c109559e
Prakash Jhunjhunwala <prakash.j@gmail.com>
2013-07-04 17:37:22 -0500

    modified web.js

--- web.js
+++ web.js
@@ -2,9 +2,14 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var filename="index.html";
+var fs= require('fs');
+var data = fs.readFileSync(filename, "utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(data);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
